a0b13ba113fb18c344ecc607df7ce9c20954d4da
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-09-24 03:53:46 -0300

    feat: implements logic to balance gifts

--- src/main/java/ifsp/macena/SledBalancer.java
+++ src/main/java/ifsp/macena/SledBalancer.java
@@ -1,4 +1,19 @@
 package ifsp.macena;
 
 public class SledBalancer {
+    public String canBalance(int[] weights) {
+        int leftSide = 0;
+        int rightSide = 0;
+
+        for (int weight : weights) {
+            if (Math.abs((leftSide + weight) - rightSide) <= 5) {
+                leftSide += weight;
+            } else if (Math.abs(leftSide - (rightSide + weight)) <= 5) {
+                rightSide += weight;
+            } else {
+                return "N";
+            }
+        }
+        return "S";
+    }
 }
