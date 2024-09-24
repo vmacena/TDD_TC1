8f6621f5faa0c541993ecaa808159725489e046e
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-09-24 04:02:35 -0300

    test: add test for balancing sled with a single present

--- src/test/java/ifsp/macena/SledBalancerTest.java
+++ src/test/java/ifsp/macena/SledBalancerTest.java
@@ -1,4 +1,17 @@
 package ifsp.macena;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SledBalancerTest {
+
+    @Test
+    void testBalancedWithSinglePresent() {
+        SledBalancer sledBalancer = new SledBalancer();
+        int[] weights = {3};
+        assertEquals(
+                "S", sledBalancer.isBalanced(weights),
+                "A single gift should always be balanced.");
+    }
 }
